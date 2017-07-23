74de509a777f0683d33f3ce71894524dbdae47d7
LarrySong <Song@Songs-MacBook-Pro.local>
2017-07-23 00:26:52 -0400

    BubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(intA[], int n){
+	for(int k = 0; k < n; k++){
+		bool swapped = false
+		for(int i = 0; i < n - k; i++){
+			if(A[i] > A[i + 1]){
+				int temp = A[i];
+				A[i] = A[i + 1];
+				A[i + 1] = temp;
+				swapped = true
+			}		
+			if(swapped == false)
+				break;
+		}
+	}
+}
